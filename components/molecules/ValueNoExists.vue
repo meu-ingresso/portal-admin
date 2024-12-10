f612b8ad0245cdd87ce857080d22e11e08e366e8
Kelvyn Krug <kelvynkrug@gmail.com>
2024-12-10 20:17:32 -0300

    feat: added user validation

---
+++ components/molecules/ValueNoExists.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <v-alert dense outlined type="error">
+      {{ text }}
+    </v-alert>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+
+  methods: {
+    back(): void {
+      this.$router.replace(this.to);
+    },
+  },
+});
+</script>
