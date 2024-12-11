a0b8bee4387eace1d222c6598b71dd9aeec6315b
Vinícius Machado <viniciusadrianomachado@gmail.com>
2024-12-10 21:54:34 -0300

    feat: Event details better date, and page for tickets

---
+++ components/molecules/ticket/TicketStatistics.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row class="mb-4">
+    <v-col cols="12">
+      <div class="tickets-statistics-title">Ingressos</div>
+    </v-col>
+
+    <v-col
+      v-for="stat in statistics"
+      :key="stat.title"
+      cols="12"
+      xl="4"
+      lg="4"
+      md="6"
+      sm="12">
+      <StatisticCard :title="stat.title" :value="stat.value" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    statistics: { type: Array, required: true },
+  },
+};
+</script>
+<style scoped>
+.tickets-statistics-title {
+  font-size: 40px;
+  font-weight: 700;
+  color: var(--black-text);
+  font-family: var(--font-family-poppins-bold);
+}
+</style>
